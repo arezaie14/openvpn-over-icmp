832b36572f7aebed52adecf6b11d26c73dca4758
Amin Rezaie <arezaie14@gmail.com>
2025-06-29 15:52:48 +0330

    updated

--- server/tinyproxy/entrypoint.sh
+++ server/tinyproxy/entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-# Replace placeholders in the config template with env values
+# Find the actual path of tinyproxy
+TINYPROXY_BIN=$(which tinyproxy)
+
+if [ ! -x "$TINYPROXY_BIN" ]; then
+  echo "Error: tinyproxy binary not found!"
+  exit 1
+fi
+
+# Generate config
 envsubst < /etc/tinyproxy/tinyproxy.conf.template > /etc/tinyproxy/tinyproxy.conf
 
-# Start Tinyproxy in the foreground
-/usr/sbin/tinyproxy -d
+# Run tinyproxy
+exec "$TINYPROXY_BIN" -d
